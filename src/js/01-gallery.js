15172a570b22cd241ee5c5cf6b5506c12d3f55c0
Lora <lorat1403@gmail.com>
2022-07-07 23:13:36 +0300

    case1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+// import * as basicLightbox from "basiclightbox";
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+// galleryEl.addEventListener("click", onGalleryClick);
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div> `;
+  })
+  .join("");
+console.log(galleryMarkup);
+
+galleryEl.innerHTML = galleryMarkup;
+
+// function onGalleryClick(event) {
+//   event.preventDefault();
+//   if (event.target.classList.contains(".gallery__image")) {
+//     return;
+//   }
+//   const instance = basicLightbox.create(
+//     ` <div class="modal"><img src="${event.target.dataset.source}"></div>`
+//   );
+//   instance.show();
+// }
