3c9b2fcdd3cd44d41cc37e3422900d0df0e71d3a
Lora <lorat1403@gmail.com>
2022-07-13 18:16:08 +0300

    gall-work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// import * as basicLightbox from "basicLightbox";
+
 console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
@@ -19,9 +19,29 @@ galleryEl.innerHTML = galleryMarkup;
 function onGalleryClick(event) {
   event.preventDefault();
   console.log("I'm working");
+  if (event.target.classList.contains(".gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox
+    .create(
+      ` <div class="modal"><img src="${event.target.dataset.source}"></div>`,
+      {
+        onShow: () => {
+          galleryEl.addEventListener("keydown", onEscPress),
+            document.body.classList.add("no-scroll");
+        },
+        onClose: () => {
+          galleryEl.addEventListener("keydown", onEscPress),
+            document.body.classList.remove("no-scroll");
+        },
+      }
+    )
+    .show();
 
-  const instance = basicLightbox.create(
-    ` <div class="modal"><img src="${event.target.dataset.source}"></div>`
-  );
-  instance.show();
+  function onEscPress(event) {
+    const ESC_KEY_CODE = "Escape";
+    if (event.code === ESC_KEY_CODE) {
+      instance.close();
+    }
+  }
 }
