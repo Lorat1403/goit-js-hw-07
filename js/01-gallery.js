ec0f740095fb7a88044a15a86206174d234824b1
Lora <lorat1403@gmail.com>
2022-07-16 16:09:01 +0300

    upd-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,29 +18,22 @@ galleryEl.innerHTML = galleryMarkup;
 
 function onGalleryClick(event) {
   event.preventDefault();
-  console.log("I'm working");
+
   if (event.target.classList.contains(".gallery__image")) {
     return;
   }
-  const instance = basicLightbox
-    .create(
-      ` <div class="modal"><img src="${event.target.dataset.source}"></div>`,
-      {
-        onShow: () => {
-          galleryEl.addEventListener("keydown", onEscPress),
-            document.body.classList.add("no-scroll");
-        },
-        onClose: () => {
-          galleryEl.addEventListener("keydown", onEscPress),
-            document.body.classList.remove("no-scroll");
-        },
-      }
-    )
-    .show();
-}
-function onEscPress(event) {
-  const ESC_KEY_CODE = "Escape";
-  if (event.code === ESC_KEY_CODE) {
-    instance.close();
+  const instance = basicLightbox.create(
+    ` <div class="modal"><img src="${event.target.dataset.source}"></div>`
+  );
+
+  instance.show();
+
+  window.addEventListener("keydown", onEscPress);
+
+  function onEscPress(event) {
+    const ESC_KEY_CODE = "Escape";
+    if (event.code === ESC_KEY_CODE) {
+      return instance.close();
+    }
   }
 }
